8c8d18f45091da8eadcb228658b4be94559be76a
Ricardo Barantini <ricardobarantini@gmail.com>
2018-04-30 23:59:59 -0300

    Select newly created note. Sort notes by updatedAt.

--- imports/ui/NoteListHeader.js
+++ imports/ui/NoteListHeader.js
@@ -2,21 +2,28 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { Session } from 'meteor/session';
 
 export const NoteListHeader = (props) => {
   return (
     <button onClick={() => {
-      props.meteorCall('notes.insert');
+      props.meteorCall('notes.insert', (err, res) => {
+        if (res) {
+          props.Session.set('selectedNoteId', res);
+        }
+      });
     }}>Create Note</button>
   );
 };
 
 NoteListHeader.propTypes = {
-  meteorCall: PropTypes.func.isRequired
+  meteorCall: PropTypes.func.isRequired,
+  Session: PropTypes.object.isRequired
 };
 
 export default createContainer(() => {
   return {
-    meteorCall: Meteor.call
+    meteorCall: Meteor.call,
+    Session
   };
 }, NoteListHeader);
